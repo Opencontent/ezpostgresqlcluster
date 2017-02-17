f53ae7cc830ce3494842c2c71fa91e2ef1c3d1ce
Luca Realdi <lr@opencontent.it>
2017-02-17 15:55:06 +0100

    fix scope varchar limit

--- sql/postgresql/cluster_dfs_schema.sql
+++ sql/postgresql/cluster_dfs_schema.sql
@@ -3,7 +3,7 @@ CREATE TABLE ezdfsfile (
     name        text            NOT NULL,
     name_trunk  text            NOT NULL,
     name_hash   character(32)   DEFAULT '' NOT NULL PRIMARY KEY,
-    scope       varchar(20)     DEFAULT '' NOT NULL,
+    scope       varchar(100)     DEFAULT '' NOT NULL,
     size        integer         DEFAULT 0 NOT NULL,
     mtime       integer         DEFAULT 0 NOT NULL,
     expired     integer         DEFAULT 0 NOT NULL
@@ -17,7 +17,7 @@ CREATE TABLE ezdfsfile_cache (
     name        text            NOT NULL,
     name_trunk  text            NOT NULL,
     name_hash   character(32)   DEFAULT '' NOT NULL PRIMARY KEY,
-    scope       varchar(20)     DEFAULT '' NOT NULL,
+    scope       varchar(100)     DEFAULT '' NOT NULL,
     size        integer         DEFAULT 0 NOT NULL,
     mtime       integer         DEFAULT 0 NOT NULL,
     expired     integer         DEFAULT 0 NOT NULL
@@ -25,4 +25,3 @@ CREATE TABLE ezdfsfile_cache (
 
 CREATE INDEX ezdfsfile_cache_name  ON ezdfsfile_cache ( name );
 CREATE INDEX ezdfsfile_cache_mtime ON ezdfsfile_cache ( mtime );
-
---
+++ sql/update/schema.sql
@@ -0,0 +1,2 @@
+ALTER TABLE ezdfsfile ALTER COLUMN scope TYPE varchar(100);
+ALTER TABLE ezdfsfile_cache ALTER COLUMN scope TYPE varchar(100);
